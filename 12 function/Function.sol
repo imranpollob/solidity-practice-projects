33e5f690c0ebb449dc754526e122953c8d510954
imranpollob <polboy777@gmail.com>
2022-11-06 16:24:56 -0500

    funstion

---
+++ 12 function/Function.sol
@@ -0,0 +1,190 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Function {
+    // Functions can return multiple values.
+    function returnMany()
+        public
+        pure
+        returns (
+            uint,
+            bool,
+            uint
+        )
+    {
+        return (1, true, 2);
+    }
+
+    // Return values can be named.
+    function named()
+        public
+        pure
+        returns (
+            uint x,
+            bool b,
+            uint y
+        )
+    {
+        return (1, true, 2);
+    }
+
+    // Return values can be assigned to their name.
+    // In this case the return statement can be omitted.
+    function assigned()
+        public
+        pure
+        returns (
+            uint x,
+            bool b,
+            uint y
+        )
+    {
+        x = 1;
+        b = true;
+        y = 2;
+    }
+
+    // Use destructuring assignment when calling another
+    // function that returns multiple values.
+    function destructuringAssignments()
+        public
+        pure
+        returns (
+            uint,
+            bool,
+            uint,
+            uint,
+            uint
+        )
+    {
+        (uint i, bool b, uint j) = returnMany();
+
+        // Values can be left out.
+        (uint x, , uint y) = (4, 5, 6);
+
+        return (i, b, j, x, y);
+    }
+
+    // Cannot use map for either input or output
+
+    // Can use array for input
+    function arrayInput(uint[] memory _arr) public {}
+
+    // Can use array for output
+    uint[] public arr;
+
+    function arrayOutput() public view returns (uint[] memory) {
+        return arr;
+    }
+}
+
+// Call function with key-value inputs
+contract XYZ {
+    function someFuncWithManyInputs(
+        uint x,
+        uint y,
+        uint z,
+        address a,
+        bool b,
+        string memory c
+    ) public pure returns (uint) {}
+
+    function callFunc() external pure returns (uint) {
+        return someFuncWithManyInputs(1, 2, 3, address(0), true, "c");
+    }
+
+    function callFuncWithKeyValue() external pure returns (uint) {
+        return
+            someFuncWithManyInputs({
+                a: address(0),
+                b: true,
+                c: "c",
+                x: 1,
+                y: 2,
+                z: 3
+            });
+    }
+}
+
+/*
+Getter functions can be declared view or pure.
+- View function declares that no state will be changed.
+- Pure function declares that no state variable will be changed or read.
+*/
+
+contract ViewAndPure {
+    uint public x = 1;
+
+    // Promise not to modify the state.
+    function addToX(uint y) public view returns (uint) {
+        return x + y;
+    }
+
+    // Promise not to modify or read from the state.
+    function add(uint i, uint j) public pure returns (uint) {
+        return i + j;
+    }
+}
+
+
+/*
+Modifiers are code that can be run before and / or after a function call.
+
+Modifiers can be used to:
+- Restrict access
+- Validate inputs
+- Guard against reentrancy hack
+*/
+
+contract FunctionModifier {
+    // We will use these variables to demonstrate how to use
+    // modifiers.
+    address public owner;
+    uint public x = 10;
+    bool public locked;
+
+    constructor() {
+        // Set the transaction sender as the owner of the contract.
+        owner = msg.sender;
+    }
+
+    // Modifier to check that the caller is the owner of
+    // the contract.
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        // Underscore is a special character only used inside
+        // a function modifier and it tells Solidity to
+        // execute the rest of the code.
+        _;
+    }
+
+    // Modifiers can take inputs. This modifier checks that the
+    // address passed in is not the zero address.
+    modifier validAddress(address _addr) {
+        require(_addr != address(0), "Not valid address");
+        _;
+    }
+
+    function changeOwner(address _newOwner) public onlyOwner validAddress(_newOwner) {
+        owner = _newOwner;
+    }
+
+    // Modifiers can be called before and / or after a function.
+    // This modifier prevents a function from being called while
+    // it is still executing.
+    modifier noReentrancy() {
+        require(!locked, "No reentrancy");
+
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    function decrement(uint i) public noReentrancy {
+        x -= i;
+
+        if (i > 1) {
+            decrement(i - 1);
+        }
+    }
+}

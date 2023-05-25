61f7fbb1b39209663de5f4e7f6b34b87e0b4292c
Imran Pollob <polboy777@gmail.com>
2023-05-25 05:16:27 -0400

    try catch

--- 22 try catch/TryCatch.sol
+++ 22 try catch/TryCatch.sol
@@ -1,3 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity 0.8.18;
 
+contract Test {
+    function willFail() public pure {
+        require(false, "It's failing");
+    }
+}
+
+contract TryCatchError {
+    event Log(string _msg);
+
+    function test(address _address) external {
+        Test t = Test(_address);
+            try t.willFail() {
+        } catch Error(string memory _msg) {
+            emit Log(_msg);
+        }
+    }
+}

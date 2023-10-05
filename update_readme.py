9b409ae122fdc458518d54b964903359162035c7
imranpollob <polboy777@gmail.com>
2023-10-05 18:03:58 -0400

    update py

--- update_readme.py
+++ update_readme.py
@@ -3,50 +3,46 @@ import os
 text = ""
 base_dir = os.getcwd()
 
-def print_files_and_directories(prev_dir='', current_dir = '', depth = 0):
+
+def print_files_and_directories(prev_dir="", current_dir="", depth=0):
     global text
 
     if depth == 2:
         return
 
     depth += 1
-    
+
     for item in sorted(os.listdir(current_dir)):
         new_dir = os.path.join(current_dir, item)
-        
+
         if item.strip().startswith("."):
             continue
-        
+
         if os.path.isfile(new_dir):
-            temp = current_dir.replace(base_dir, '')
+            temp = current_dir.replace(base_dir, "")
             a = f"{'  ' * depth}- [📄 {item}]({temp}/{item})\n"
             print(a)
             text += a
         elif os.path.isdir(new_dir):
-            temp = current_dir.replace(base_dir, '')
+            temp = current_dir.replace(base_dir, "")
             b = f"{'  ' * depth}- [📂 {item}]({temp}/{item})\n"
             print(b)
             text += b
             print_files_and_directories(current_dir, new_dir, depth)
-                     
+
 
 if __name__ == "__main__":
-    print_files_and_directories('', base_dir, 0)
-    
-    print(base_dir)
-    print()
+    print_files_and_directories("", base_dir, 0)
 
-    with open("README.md", "r", encoding="utf-8") as f:
+    with open("readme.md", "r", encoding="utf-8") as f:
         content = f.read()
-        
+
     start_index = content.find("## Folder Structure")
     end_index = content.rfind("### Helpful resources:")
-    
+
     if start_index != -1:
         text = "## Folder Structure\n" + text + "\n\n"
         content = content[:start_index] + text + content[end_index:]
-        
-        with open("README.md", "w", encoding="utf-8") as f:
+
+        with open("readme.md", "w", encoding="utf-8") as f:
             f.write(content)
-    
-    
